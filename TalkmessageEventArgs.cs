a9d3741380fc08456123ee95a0ca9637b26ffc83
williamd4112 <williamd4112@hotmail.com>
2014-03-09 18:57:05 +0800

    Alpha 2

---
+++ TalkmessageEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkLibrary
+{
+    public class TalkmessageEventArgs:EventArgs
+    {
+        private string message;
+        private string nickname;
+        private string messageType;
+        private Talkconnection connection;
+
+        public string Message { get { return message; } }
+        public string Nickname { get { return nickname; } }
+        public string MessageType { get { return messageType; } }
+        public Talkconnection Connection { get { return connection; } }
+
+        public TalkmessageEventArgs(string message, string nickname , string messageType, Talkconnection connection)
+            : base()
+        {
+            this.message = message;
+            this.nickname = nickname;
+            this.connection = connection;
+            this.messageType = messageType;
+        }
+
+        
+    }
+}
